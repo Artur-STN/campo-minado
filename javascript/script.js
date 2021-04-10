9f1c996c13dfc8f91dd4e27febc2996511f772da
Artur Santos <artur_teixeira10@hotmail.com>
2021-04-10 20:22:14 -0300

    code

--- javascript/script.js
+++ javascript/script.js
@@ -3,8 +3,8 @@ function insert(paramsBackground, paramsFavicon) {
     document.querySelector('body').style.background = paramsBackground
     document.getElementById('span').style.color = paramsBackground
 
-    const favicon = document.getElementById('favicon')
+    console.log(`/image/favicon-${paramsFavicon}.ico`)
     
-    favicon.setAttribute("href", `../image/favicon-${paramsFavicon}.png`)
+    document.querySelector("link[rel='shortcut icon']").setAttribute("href",`/image/favicon-${paramsFavicon}.ico`);
     
 }
